0f710609f58b919860e0ae2bbbf65d57c6af63e4
Valmiki Vishwakarma <you@example.com>
2022-12-10 00:26:24 +0530

    Adding Object array example.

---
+++ JavaCoding/src/Dec09/TestArray.java
@@ -0,0 +1,30 @@
+package Dec09;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		// Object Array
+		Object[] arr = { "steve", 100, 29392.238, 23238.282239f, 33723888993L };
+
+		System.out.println("Array Length is : " + arr.length);
+
+		// Print all the Array elements
+
+		for (Object ar : arr) {
+			System.out.println(ar);
+		}
+		
+		/*
+		Array Length is : 5
+		steve
+		100
+		29392.238
+		23238.283
+		33723888993
+		 			*/
+		
+		
+
+	}
+
+}
